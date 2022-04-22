e11f6fe116e761c5bc4bdd32b356d577454ab54a
Ale <alexander.grishinn@yandex.ru>
2022-04-22 21:10:18 +0300

    new attempt

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -1,96 +1,82 @@
-#1 Fizz Buzz
-''''''
-def fizz_buzz(value1, value2):
-    """ Creates list of values in range between value1 and value2 including value2,
-    then searches which ones can be divided by 3 and 5,
-    then returns sum of those numbers
+#Функция fizz_buzz
+def fizz_buzz(a, b):
+    result = 0
+    list = []
+    for i in range(a, b+1):
+        if i % 3 == 0 and i % 5 == 0:
+            list.append(i)
+    result = sum(list)    
+    return result
 
-    :param a: int. First value
-    :param b: int. Second value
-    :return : sum of values divisible by 3 and 5 in range between value1 and value2 (value2 included)
-    """
-    interval = []
-    for value in range(value1, value2 + 1):
-        interval.append(value)
-        
-    i = 0
-    fizz_buzz_list = []
-    while i < len(interval):
-        value_checked = interval[i]
-        if value_checked % 3 == 0 and value_checked % 5 == 0:
-            fizz_buzz_list.append(value_checked)
-            i += 1
+print('0-3:', fizz_buzz(0, 3))
+print('0-5:', fizz_buzz(0, 5))
+print('15-15:', fizz_buzz(15, 15))
+print('1000-20000:', fizz_buzz(1000, 20000))
+
+
+#Функция plural_form
+def plural_form(a, form1, form2, form3):
+    result = ''
+    n = list(str(a))
+    if int(n[-1]) == 1:
+        if len(n) >= 2:
+            if int(n[-2]) == 1:
+                result = form3
+            else:
+                result = form1
         else:
-            i += 1
-                
-    return sum(fizz_buzz_list)
+                result = form1           
+    elif int(n[-1]) in range(2, 5):
+        result = form2
+    elif int(n[-1]) in range(5, 21):
+        result = form3
+    return result
 
-print('0-3: ', fizz_buzz(0, 3))
-print('0-5: ', fizz_buzz(0, 5))
-print('15-15: ', fizz_buzz(15, 15))
-print('1000-20000: ', fizz_buzz(1000, 20000))
-
-
-#2 Plural
-def plural_form(value1, value2, value3, value4):
-    """Return correct single or plural form of the word depending on number
-
-    :param a: int, First value
-    :param b: str, Second value
-    :param c: str, Third value
-    :param d: str, 4th value
-    :result: correct plural (or single) form of the word fitting the number
-    """
-    if value1 == 11 or value1 == 12:
-        result = value4
-    elif str(value1)[-1] == '1':
-        result = value2
-    elif str(value1)[-1] == '2' or str(value1)[-1] == '3' or str(value1)[-1] == '4':
-        result = value3
-    else:
-        result = value4
-
-    return result    
-        
 print(1, plural_form(1, 'яблоко', 'яблока', 'яблок'))
 print(3, plural_form(3, 'яблоко', 'яблока', 'яблок'))
 print(5, plural_form(5, 'яблоко', 'яблока', 'яблок'))
 print(11, plural_form(11, 'яблоко', 'яблока', 'яблок'))
 print(121, plural_form(121, 'яблоко', 'яблока', 'яблок'))
-print(125, plural_form(125, 'яблоко', 'яблока', 'яблок'))        
+print(125, plural_form(125, 'яблоко', 'яблока', 'яблок'))
+
+
+#Декоратор html
+def html(a, width='', height='', href=''):
+
+    def decorator(decorated_function):
+
+        def function_inside_decorator(input_value):
+            
+            result_decorated_function = f'<{a}>{decorated_function(input_value)}</{a}>'
+
+            if width and height:
+                result_decorated_function = f'<{a} width="{width}" heigth="{height}">{decorated_function(input_value)}</{a}>'
+            elif width:
+                result_decorated_function = f'<{a} width="{width}">{decorated_function(input_value)}</{a}>'    
+            elif height:
+                result_decorated_function = f'<{a} height="{height}">{decorated_function(input_value)}</{a}>'
+                
+            if href:
+                result_decorated_function = f'<{a} href="{href}">{decorated_function(input_value)}</{a}>'              
+            
+            return result_decorated_function
+    
+        return function_inside_decorator
+    
+    return decorator
+
+
+@html('body')
+@html('div', width=200, height=100)
+@html('a', href='https://yandex.ru/')
+def to_string(input_value):
+    return str(input_value)
+
+
+print(to_string('ссылка на яндекс'))
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+   
+    
